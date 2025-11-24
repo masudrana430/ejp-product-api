705a93af4c352846e793a5327f055f7df06ead1c
masud rana <masud040502@gmail.com>
2025-11-24 22:21:20 +0600

    m

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000" || "https://ejp-next-js-project.netlify.app" ;
 
 app.use(
   cors({
